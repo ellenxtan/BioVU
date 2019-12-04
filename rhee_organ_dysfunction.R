eb23f97e198c69084a9575cb4ea7ed1980fd2196
Ding <FED11@pitt.edu>
2019-12-04 12:52:01 -0500

    Add data to Rhee definition 1. Exclude patients with end-stage kidney disease and calculate eGFR 2. Compare with Sepsis code. 3. Add new .Rmd file

--- rhee_organ_dysfunction.R
+++ rhee_organ_dysfunction.R
@@ -5,8 +5,7 @@ library(lubridate)
 
 rhee_infection <- read_csv("../output/rhee_infection_20191015.csv")
 changed_grid <- read_csv("../output/changed_grid_dob_20190924.csv")
-static_raw <- read_csv("../../Mito Delirium BioVU Data/Data/Samuels_Delirium_STATIC_20180718.csv") %>% 
-  select(GRID)
+static_raw <- read_csv("../../Mito Delirium BioVU Data/Data/Samuels_Delirium_STATIC_20180718.csv") 
 names(static_raw) <- str_to_lower(names(static_raw))
 
 #. Vasopressor initiation ------------------
@@ -168,7 +167,8 @@ lactate_ge2 <- sqldf::sqldf('SELECT *
   distinct(grid, adm_id, adm_date, dc_date, blood_date) %>%
   mutate(lactate_ge2 = 1)
 
-#. creatinine doubling ------------------------
+#. renal dysfunction ------------------------
+#> creatinine doubling ----
 file_names <- list.files("../../Mito Delirium BioVU Data/Lab values/Creatinine",
                          full.names = T)
 
@@ -221,35 +221,156 @@ creatinine_raw %>%
   filter(`creat mg/dl` > 50)
 
 
-# find baseline for each hospitaliztion, then filter to find creatinine doubling within 2 day window 
-creatinine_dbl <- sqldf::sqldf('SELECT * 
+
+
+#> calculate EGFR---------------
+## get sex, race from static_raw and convert messed-up GRIDs
+static_raw %>% 
+  select(sex, race) %>% 
+  Hmisc::describe()
+demo_raw <- static_raw %>% 
+  select(grid, sex, race, dob) %>%
+  left_join(changed_grid, by = c("grid" = "old_grid")) %>% 
+  mutate(
+    grid = if_else(!is.na(updated_grid), updated_grid, grid),
+    dob = if_else(!is.na(updated_dob), updated_dob, mdy(dob))
+  ) %>% 
+  distinct(grid, sex, race, dob) %>% 
+  arrange(grid)
+sum(demo_raw$grid %in% changed_grid$old_grid)
+demo_raw %>% 
+  Hmisc::describe()
+demo_raw %>% 
+  group_by(grid) %>% 
+  filter(n_distinct(race) > 1) %>% 
+  print(n = 100)
+demo_raw %>% 
+  group_by(grid) %>% 
+  filter(n_distinct(sex) > 1) %>% 
+  print(n = 100)
+demo_raw %>% 
+  group_by(grid) %>% 
+  filter(n_distinct(dob) > 1) %>% 
+  print(n = 100)
+demo <- demo_raw %>% 
+  group_by(grid) %>% 
+  mutate(
+    race_b = case_when(
+      sum(str_detect(race, "B")) > 0 ~ "Black",
+      sum(race == "U") != n() ~ "White or other")
+    ) %>% 
+  ungroup() %>% 
+  distinct(grid, sex, race_b, dob) %>% 
+  mutate(
+    sex = case_when(
+      sex != "U" ~ sex)
+    )
+demo %>% Hmisc::describe()
+
+## Calculate age at a given reference date 
+calc_age <- function(birthDate, refDate = Sys.Date()) {
+  period <- as.period(interval(birthDate, refDate),
+                      unit = "year")
+  
+  period$year
+  
+}
+
+egfr_raw <- creatinine_raw %>% 
+  select(-oor_value, -lab_time) %>% 
+  left_join(demo, by = "grid") %>% 
+  mutate(
+    age = calc_age(dob, lab_date),
+    egfr = case_when(
+      race_b == "Black" & sex == "F" & `creat mg/dl` <= 0.7 ~ 166*(`creat mg/dl`/0.7)^(-0.329)*(0.993)^age,
+      race_b == "Black" & sex == "F" & `creat mg/dl` >  0.7 ~ 166*(`creat mg/dl`/0.7)^(-1.209)*(0.993)^age,
+      race_b == "Black" & sex == "M" & `creat mg/dl` <= 0.9 ~ 163*(`creat mg/dl`/0.9)^(-0.411)*(0.993)^age,
+      race_b == "Black" & sex == "M" & `creat mg/dl` >  0.9 ~ 163*(`creat mg/dl`/0.9)^(-1.209)*(0.993)^age,
+      race_b == "White or other" & sex == "F" & `creat mg/dl` <= 0.7 ~ 144*(`creat mg/dl`/0.7)^(-0.329)*(0.993)^age,
+      race_b == "White or other" & sex == "F" & `creat mg/dl` >  0.7 ~ 144*(`creat mg/dl`/0.7)^(-1.209)*(0.993)^age,
+      race_b == "White or other" & sex == "M" & `creat mg/dl` <= 0.9 ~ 141*(`creat mg/dl`/0.9)^(-0.411)*(0.993)^age,
+      race_b == "White or other" & sex == "M" & `creat mg/dl` >  0.9 ~ 141*(`creat mg/dl`/0.9)^(-1.209)*(0.993)^age
+    ),
+    egfr_60 = if_else(egfr > 60, 60, egfr)
+    )
+egfr_raw %>% 
+  Hmisc::describe()
+ggplot(egfr_raw) +
+  geom_histogram(aes(x = egfr)) 
+egfr_raw %>% 
+  filter(egfr > 60) # more than half
+egfr_raw %>% 
+  filter(egfr == Inf)
+
+#> exclude end-stage kidney disease --------------------------
+esrd_raw <- read_tsv("../../Mito Delirium BioVU Data/Phenotype data/ESRD_ICD.txt")
+names(esrd_raw) <- str_to_lower(names(esrd_raw))
+esrd_raw %<>% 
+  mutate(esrd_code_date = mdy(esrd_code_date))
+esrd_raw %>% 
+  Hmisc::describe() # 4705 pts
+
+
+# check for messed-up GRID
+length(unique(creatinine_raw$grid))
+sum(unique(esrd_raw$grid) %in% unique(static_raw$grid))  # check whether any new updated GRID
+sum(unique(esrd_raw$grid) %in% unique(changed_grid$old_grid)) # no OLD GRID
+sum(unique(esrd_raw$grid) %in% unique(changed_grid$updated_grid))
+
+rhee_infection_ex <- sqldf::sqldf('SELECT * 
               FROM rhee_infection as t1
-              INNER JOIN creatinine_raw as t2 
-              ON t1.grid = t2.grid AND lab_date BETWEEN adm_date-3 AND dc_date') %>% 
+              INNER JOIN esrd_raw as t2 
+              ON t1.grid = t2.grid AND esrd_code_date BETWEEN adm_date - 3 AND dc_date') %>% 
   as_tibble() %>% 
-  select(grid, adm_id, adm_date, dc_date, blood_date, lab_date, `creat mg/dl`) %>% 
+  distinct(grid, adm_id, adm_date, dc_date, blood_date)
+rhee_infection1 <- rhee_infection  %>% 
+  anti_join(rhee_infection_ex)
+
+#> identify creatinine doubling/edfr decline infections -------------------- 
+# find baseline for each hospitaliztion, then filter to find creatinine doubling within 2 day window
+renal_dysfct <- sqldf::sqldf('SELECT * 
+                               FROM rhee_infection1 as t1
+                               INNER JOIN egfr_raw as t2 
+                               ON t1.grid = t2.grid AND lab_date BETWEEN adm_date-3 AND dc_date') %>% 
+  as_tibble() %>% 
+  select(grid, adm_id, adm_date, dc_date, blood_date, lab_date, `creat mg/dl`, sex, race_b, age, egfr, egfr_60) %>% 
   group_by(grid, adm_id) %>% 
-  mutate(creat_bl = min(`creat mg/dl`)) %>% 
+  mutate(creat_bl = min(`creat mg/dl`),
+         egfr_bl = max(egfr_60)
+         ) %>% 
   ungroup() %>% 
   group_by(grid, adm_id, adm_date, dc_date, blood_date) %>% 
   filter(lab_date >= blood_date - 2, 
          lab_date <= blood_date + 2,
-         `creat mg/dl` >= 2*creat_bl) %>% 
+         `creat mg/dl` >= 2*creat_bl | egfr_60 <= 0.5*egfr_bl) %>% 
   ungroup()
 ## check range of creatinine
-creatinine_dbl %>% 
-  select(creat_bl, `creat mg/dl`) %>% 
+renal_dysfct %>% 
+  select(`creat mg/dl`:egfr_bl) %>% 
   Hmisc::describe()
-creatinine_dbl %>% 
+renal_dysfct %>% 
   filter(`creat mg/dl` < 1.2)
-creatinine_dbl %>% 
+renal_dysfct %>% 
   filter(`creat mg/dl` < 1.2) %>% 
   distinct(grid, adm_id, adm_date, dc_date, creat_bl)
+## check egfr
+renal_dysfct %>% 
+  filter(egfr > 60)
 # get distinct BC dates
-creatinine_dbl_date <- creatinine_dbl %>% 
+renal_dysfct_date <- renal_dysfct %>% 
   distinct(grid, adm_id, adm_date, dc_date, blood_date) %>% 
-  mutate(creat_dbl  = 1)
+  mutate(renal_dysfct  = 1)
 
+# check End Stage Renal Disease Status
+sqldf::sqldf('SELECT * 
+              FROM renal_dysfct_date as t1
+              INNER JOIN esrd_raw as t2 
+              ON t1.grid = t2.grid AND adm_date > esrd_code_date') %>% 
+  as_tibble() %>% 
+  distinct(grid) #123 pts
+esrd_raw %>% 
+  filter(grid == "R200617056") %>% 
+  arrange(esrd_code_date)
 
 #. bilirubin doubling --------------------------------------------------------
 file_names <- list.files("../../Mito Delirium BioVU Data/Lab values/Bilirubin",
@@ -418,48 +539,112 @@ platelet_dcl_date <- platelet_dcl %>%
 
 
 
+
+
 #. Put everything together -----------------------------------------------------
 ## keep the first blood date with the worst organ dysfunction value for each encounter
 rhee_sepsis <- rhee_infection %>% 
   left_join(pressor_new) %>% 
   left_join(vent_new) %>% 
-  left_join(creatinine_dbl_date) %>% 
+  left_join(renal_dysfct_date) %>% 
   left_join(bilirubin_dbl_date) %>% 
   left_join(platelet_dcl_date) %>% 
   left_join(lactate_ge2) %>% 
-  mutate(rhee = if_else(is.na(new_pressor | new_vent | creat_dbl | bil_dbl | plt_dcl | lactate_ge2), 0, 1)) %>% 
+  mutate(rhee = if_else(is.na(new_pressor | new_vent | renal_dysfct | bil_dbl | plt_dcl | lactate_ge2), 0, 1)) %>% 
   group_by(grid, adm_id, adm_date, dc_date) %>% 
   slice(which.max(rhee)) %>% 
   ungroup()
 rhee_sepsis %>% 
   count(rhee)
-write_csv(rhee_sepsis, "../output/sepsis_rhee_20191115.csv")
+write_csv(rhee_sepsis, "../output/sepsis_rhee_20191203.csv")
 
 
-#. compare with sepsis3 infection -------------------------------
+#. compare with sepsis3 infection and ICD9 code -------------------------------
 cam_visits <- read_csv("../output/cam_stay_20190925.csv") 
 sepsis3 <- read_csv("../output/sepsis3_20191014.csv")
+sepsis_code_raw <- read_excel("../../Mito Delirium BioVU Data/Phenotype data/Sepsis Code Days.xlsx") %>% 
+  mutate(`sepsis CODE_DATE` = as_date(`sepsis CODE_DATE`))
 
+names(sepsis_code_raw) <- str_to_lower(names(sepsis_code_raw))
+sepsis_code <- sqldf::sqldf('SELECT *
+             FROM cam_visits as t1
+             INNER JOIN sepsis_code_raw as t2
+             ON t1.grid = t2.grid AND `sepsis code_date` BETWEEN adm_date-1 AND dc_date') %>% 
+  as_tibble() %>% 
+  select(grid, adm_id, adm_date, dc_date, `sepsis code_date`) %>% 
+  mutate(sepsis_day = as.numeric(`sepsis code_date` - adm_date + 1)) %>% 
+  group_by(grid, adm_id) %>% 
+  summarise(sepsis_code_adm = 1,
+            sepsis_code_w1d = sum(sepsis_day %in% 0:2) > 0) %>% 
+  ungroup()
+sepsis_code %>% 
+  Hmisc::describe()
+
+
+
+rhee_sepsis %>% count(rhee)
 sepsis3 %>% count(sepsis3)
-
-
 sepsis_comp <- cam_visits %>% 
   select(grid, adm_id) %>% 
   left_join(sepsis3 %>% 
               select(grid, adm_id, sofa, sepsis3, data_type)) %>% 
   left_join(rhee_sepsis %>% 
               select(grid, adm_id, rhee)) %>%  
+  left_join(sepsis_code) %>% 
   mutate(sepsis3_infection = if_else(is.na(sepsis3), 0, 1),
          sepsis3 = if_else(is.na(sepsis3), 0, sepsis3),
          rhee_infection = if_else(is.na(rhee), 0, 1),
-         rhee = if_else(is.na(rhee), 0, rhee))
+         rhee = if_else(is.na(rhee), 0, rhee),
+         sepsis_code_adm = if_else(is.na(sepsis_code_adm), 0, 1),
+         sepsis_code_w1d = if_else(sepsis_code_w1d %in% 1, 1, 0)
+         )
+sepsis_comp %>% Hmisc::describe()
+
 xtabs(~ sepsis3_infection + rhee_infection, data = sepsis_comp, addNA = T)
 xtabs(~ sepsis3 + rhee, data = sepsis_comp, addNA = T)
+xtabs(~ sepsis_code_adm + rhee, data = sepsis_comp, addNA = T)
+xtabs(~ sepsis_code_w1d + rhee, data = sepsis_comp, addNA = T)
+xtabs(~ sepsis_code_adm + sepsis3, data = sepsis_comp, addNA = T)
+xtabs(~ sepsis_code_adm + sepsis3, data = sepsis_comp, addNA = T)
 sepsis_comp %>% 
   filter(rhee_infection == 1) %>% 
   with(xtabs(~sepsis3 + rhee))
 
 sepsis_comp %>% filter(rhee == 1, sepsis3 == 0) %>% count(data_type)
-
+write_csv(sepsis_comp, "../output/sepsis_compare_20191203.csv")
 ## All rhee infections are sepsis3 infections
 
+#. Get distinct GRID ---------------------
+dob_dis <- read_csv("../output/dob_discrepancy.csv") 
+sepsis_comp %<>% anti_join(dob_dis, by = "grid")
+rhee_sepsis %<>% anti_join(dob_dis, by = "grid")
+rhee_grid <- sepsis_comp %>% 
+  filter(rhee == 1) %>% 
+  distinct(grid)
+rhee_sepsis3_grid <- sepsis_comp %>% 
+  filter(rhee == 1 | sepsis3 == 1) %>% 
+  distinct(grid)
+rhee_sepsis3_code_grid <- sepsis_comp %>% 
+  filter(rhee == 1 | sepsis3 == 1 | sepsis_code_w1d == 1) %>% 
+  distinct(grid)
+openxlsx::write.xlsx(list(rhee_grid, rhee_sepsis3_grid, rhee_sepsis3_code_grid),
+                     file = "../output/sepsis_grids_20191204.xlsx",
+                     sheetName = c("Rhee only", "Rhee or Sepsis-3", "Rhee or Sepsis-3 or Sepsis code"))
+
+## check sepsis-code positive other negative -------------
+sepsis_comp %>% 
+  filter(sepsis_code_w1d == 1, sepsis3 != 1, rhee != 1) %>% 
+  count(sepsis3_infection, rhee_infection)
+#' 240 had both rhee and sepsis-3 infecgtion, 99 had sepsis-3 infection only
+sepsis_comp %>% 
+  filter(sepsis_code_w1d == 1, sepsis3 != 1, rhee != 1, sepsis3_infection == 1) %>% 
+  count(data_type)
+sepsis_comp %>% 
+  filter(sepsis_code_w1d == 1, sepsis3 != 1, rhee != 1, rhee_infection == 1) 
+rhee_sepsis %>% 
+  semi_join(
+    sepsis_comp %>% 
+      filter(sepsis_code_w1d == 1, sepsis3 != 1, rhee != 1, rhee_infection == 1),
+    by = c("grid", "adm_id")
+  ) %>% 
+  print(n = 300)
